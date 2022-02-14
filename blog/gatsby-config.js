3f97f69734924956d71fc62b5be063a17e36c01d
cannizga <giuseppeacannizzojr@gmail.com>
2022-02-13 20:47:39 -0600

    Complete Activity 2

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -10,31 +10,38 @@ module.exports = {
         }
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+      `gatsby-plugin-react-helmet`,
+      `gatsby-plugin-image`,
+          {
+              resolve: 'gatsby-source-contentful',
+              options: {
+                  spaceId: `cm2q9el3kkvp`,
+                  accessToken: `mqGMFbrdDUlYvJemSYaj-Hoh4JMH9Uk36lQYnGrDsX4`,
+              },
+          },
+          {
+              resolve: `gatsby-source-filesystem`,
+              options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+              }
+          },
+     `gatsby-transformer-sharp`,
+     `gatsby-plugin-sharp`,
+          {
+              resolve: `gatsby-plugin-manifest`,
+              options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                // This will impact how browsers show your PWA/website
+                // https://css-tricks.com/meta-theme-color-and-trickery/
+                // theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+          }
+      }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
