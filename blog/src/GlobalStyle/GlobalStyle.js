982f3e8521f3fe79e2a9a1ada8da907b050098d5
Giuseppe A. Cannizzo <giuseppeacannizzojr@gmail.com>
2022-03-06 14:04:56 -0600

    Fix 3

--- blog/src/GlobalStyle/GlobalStyle.js
+++ blog/src/GlobalStyle/GlobalStyle.js
@@ -10,13 +10,13 @@ body {
     margin: 0;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    article,
-    aside,
 }
+ article,
+ aside,
 @media only screen and (max-width: 480px) {
     html {
         font-size: 100%;
     }
 }
 `
-export default GlobalStyle
+export default GlobalStyle;
